c7c83fccd93545bfe64db8e4d7e850f1ff92fb17
arvin-mf <arvinmulia55@gmail.com>
2024-07-16 00:01:14 +0700

    refactor(berita): implement api response class

--- app/Http/Controllers/api/BeritaController.php
+++ app/Http/Controllers/api/BeritaController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\api;
 
+use App\Classes\ApiResponseClass;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\BeritaCollection;
 use App\Http\Resources\BeritaResource;
 use App\Models\Berita;
 use Illuminate\Http\Request;
@@ -13,24 +15,24 @@ class BeritaController extends Controller
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'foto'   => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'judul'  => 'required',
-            'teks'   => 'required',
+            'foto' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'judul' => 'required',
+            'teks' => 'required',
         ]);
 
         if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
+            return ApiResponseClass::sendError($validator->errors(), 422);
         }
 
         $image = $request->file('foto');
         $image->storeAs('public/berita', $image->hashName());
 
         $berita = Berita::create([
-            'foto'     => $image->hashName(),
-            'judul'     => $request->judul,
-            'teks'   => $request->teks,
+            'foto' => $image->hashName(),
+            'judul' => $request->judul,
+            'teks' => $request->teks,
         ]);
 
-        return new BeritaResource(true, 'Data Berita Berhasil Ditambahkan!', $berita);
+        return ApiResponseClass::sendResponse(new BeritaResource($berita), 'Data berita berhasil ditambahkan!', 201);
     }
 }
