4e4f06947f366fccea6644a1e89e66f08c8f7fbb
Rizki Fajar <muhrizkifajar28@gmail.com>
2024-07-15 19:11:44 +0700

    chore: add api response class

---
+++ app/Classes/ApiResponseClass.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Classes;
+
+class ApiResponseClass
+{
+    public static function sendResponse($result , $message ,$code=200) {
+        $response = [
+            'success' => true,
+            'data'    => $result
+        ];
+
+        if(!empty($message)) {
+            $response['message'] = $message;
+        }
+
+        return response()->json($response, $code);
+    }
+
+    public static function sendError($message, $code) {
+        $response = [
+            'success' => false,
+            'message' => $message
+        ];
+
+        return response()->json($response, $code);
+    }
+}
