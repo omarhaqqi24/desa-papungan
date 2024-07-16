841faa1c6f4f5d8f6f4cdd33ba8ffcc6502a0265
Rizki Fajar <muhrizkifajar28@gmail.com>
2024-07-16 23:12:16 +0700

    refactor(auth): implement acces token on login function

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -4,22 +4,32 @@ namespace App\Http\Controllers\api;
 
 use App\Classes\ApiResponseClass;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public function check_login(Request $request)
+    public function login(Request $request)
     {
-        $email      = $request->email;
-        $password   = $request->password;
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
 
-        if(Auth::guard('web')->attempt(['email' => $email, 'password' => $password])) {
-            $token = $request->user()->createToken('admin-token', ['admin']);
-            return ApiResponseClass::sendResponse(['token' => $token->plainTextToken], 'Otentikasi berhasil!', 200);
-        } else {
-            return ApiResponseClass::sendError('Otentikasi gagal!', 401);
+        if ($validator->fails()) {
+            return ApiResponseClass::sendError($validator->errors(), 422);
         }
 
+        if(!Auth::attempt($request->only(['email', 'password']))) {
+            return ApiResponseClass::sendError('Kredensial tidak sesuai!', 401);
+        }
+
+        $dataUser = User::where('email', $request->email)->first();
+        $token = [
+            'token' => $dataUser->createToken('access-token')->plainTextToken,
+        ];
+        return ApiResponseClass::sendResponse($token, 'Otentikasi Berhasil!', 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Classes\ApiResponseClass;
 use App\Http\Controllers\api\AuthController;
 use App\Http\Controllers\api\BeritaController;
 use App\Models\Berita;
@@ -13,7 +14,12 @@ Route::get('/user', function (Request $request) {
 Route::get('/berita', [BeritaController::class, 'getAll']);
 Route::get('/berita/{id}', [BeritaController::class, 'getById']);
 Route::post('/berita', [BeritaController::class, 'store']);
-Route::put('/berita/{id}', [BeritaController::class, 'update']);
-Route::delete('/berita/{id}', [BeritaController::class, 'destroy']);
+Route::put('/berita/{id}', [BeritaController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/berita/{id}', [BeritaController::class, 'destroy'])->middleware('auth:sanctum');
 
-Route::post('/login', [AuthController::class, 'check_login']);
+Route::post('/login', [AuthController::class, 'login']);
+
+//fallback route for unauthorized user
+Route::get('/unauthorize', function () {
+    return ApiResponseClass::sendError('Unauthorized', 403);
+})->name('login');
