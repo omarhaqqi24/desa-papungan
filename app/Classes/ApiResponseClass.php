4a963622e54fa9a41c012d27ae45179408d9ede1
Rizki Fajar <muhrizkifajar28@gmail.com>
2024-07-16 08:15:47 +0700

    refactor: implement dynamic field in api response

--- app/Classes/ApiResponseClass.php
+++ app/Classes/ApiResponseClass.php
@@ -7,12 +7,9 @@ class ApiResponseClass
     public static function sendResponse($result , $message ,$code=200) {
         $response = [
             'success' => true,
-            'data'    => $result
+            ...(!empty($message) ? ['message' => $message] : []),
+            ...(!empty($result) ? ['data' => $result] : [])
         ];
-
-        if(!empty($message)) {
-            $response['message'] = $message;
-        }
 
         return response()->json($response, $code);
     }
