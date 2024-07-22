ec46a7a49d28596a3c73eae993dac7484d14fc57
Rizki Fajar <muhrizkifajar28@gmail.com>
2024-07-22 11:30:55 +0700

    refactor: add base api url env variable

--- app/Http/Controllers/PerangkatDesaController.php
+++ app/Http/Controllers/PerangkatDesaController.php
@@ -10,15 +10,14 @@ class PerangkatDesaController extends Controller
     public function getData(){
     $client = new Client();
 
-     $response = $client->request("GET","http://127.0.0.1:8001/api/perangkat-desa");
+     $response = $client->request("GET", env("API_BASE_URL", "http://localhost:8001") . "/api/perangkat-desa");
 
      $perangkatDesas = json_decode($response->getBody());
 
-     $response2 = $client->request("GET","http://127.0.0.1:8001/api/lembaga");
+     $response2 = $client->request("GET", env("API_BASE_URL", "http://localhost:8001") . "/api/lembaga");
     
      $lembagas = json_decode($response2->getBody());
 
      return view("pemerintahan",["perangkatDesas"=>$perangkatDesas,"lembagas"=>$lembagas]);
-    //dd($lembagas);
     }
 }
