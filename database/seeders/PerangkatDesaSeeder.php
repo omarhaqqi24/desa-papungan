708132b6df2c3dd622d69cfd15514d3186554b79
arvin-mf <arvinmulia55@gmail.com>
2024-07-24 00:11:35 +0700

    refactor: changes in seeder classes

--- database/seeders/PerangkatDesaSeeder.php
+++ database/seeders/PerangkatDesaSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Jabatan;
 use App\Models\PerangkatDesa;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,6 +16,8 @@ class PerangkatDesaSeeder extends Seeder
      */
     public function run(): void
     {
+        PerangkatDesa::truncate();
+
         Storage::deleteDirectory('perangkat-desa');
         Storage::makeDirectory('perangkat-desa');
 
@@ -23,49 +26,49 @@ class PerangkatDesaSeeder extends Seeder
         PerangkatDesa::create([
             'foto' => $faker->image(storage_path('app/public/perangkat-desa'), 500, 500, null, false),
             'nama' => 'Qudlori Setiawan',
-            'jabatan_id' => 1,
+            'jabatan_id' => (Jabatan::where('nama', 'Kepala Desa')->select('id')->get())[0]->id,
             'kontak' => $faker->phoneNumber()
         ]);
         PerangkatDesa::create([
             'foto' => $faker->image(storage_path('app/public/perangkat-desa'), 500, 500, null, false),
             'nama' => 'Bambang Setiawan',
-            'jabatan_id' => 2,
+            'jabatan_id' => (Jabatan::where('nama', 'Sekretaris Desa')->select('id')->get())[0]->id,
             'kontak' => $faker->phoneNumber()
         ]);
         PerangkatDesa::create([
             'foto' => $faker->image(storage_path('app/public/perangkat-desa'), 500, 500, null, false),
             'nama' => 'Wendhy Setiawan',
-            'jabatan_id' => 3,
+            'jabatan_id' => (Jabatan::where('nama', 'Kaur Keuangan')->select('id')->get())[0]->id,
             'kontak' => $faker->phoneNumber()
         ]);
         PerangkatDesa::create([
             'foto' => $faker->image(storage_path('app/public/perangkat-desa'), 500, 500, null, false),
             'nama' => 'Denny Setiawan',
-            'jabatan_id' => 4,
+            'jabatan_id' => (Jabatan::where('nama', 'Kaur Perencanaan dan Umum')->select('id')->get())[0]->id,
             'kontak' => $faker->phoneNumber()
         ]);
         PerangkatDesa::create([
             'foto' => $faker->image(storage_path('app/public/perangkat-desa'), 500, 500, null, false),
             'nama' => 'Slamet Setiawan',
-            'jabatan_id' => 5,
+            'jabatan_id' => (Jabatan::where('nama', 'Kasi Pemerintahan')->select('id')->get())[0]->id,
             'kontak' => $faker->phoneNumber()
         ]);
         PerangkatDesa::create([
             'foto' => $faker->image(storage_path('app/public/perangkat-desa'), 500, 500, null, false),
             'nama' => 'Bastomi Setiawan',
-            'jabatan_id' => 6,
+            'jabatan_id' => (Jabatan::where('nama', 'Kasi Kesejahteraan dan Pelayanan')->select('id')->get())[0]->id,
             'kontak' => $faker->phoneNumber()
         ]);
         PerangkatDesa::create([
             'foto' => $faker->image(storage_path('app/public/perangkat-desa'), 500, 500, null, false),
             'nama' => 'Zaenal Setiawan',
-            'jabatan_id' => 7,
+            'jabatan_id' => (Jabatan::where('nama', 'Staf Keuangan Desa')->select('id')->get())[0]->id,
             'kontak' => $faker->phoneNumber()
         ]);
         PerangkatDesa::create([
             'foto' => $faker->image(storage_path('app/public/perangkat-desa'), 500, 500, null, false),
             'nama' => 'Kurniawan Setiawan',
-            'jabatan_id' => 8,
+            'jabatan_id' => (Jabatan::where('nama', 'Staf Perencanaan dan Umum')->select('id')->get())[0]->id,
             'kontak' => $faker->phoneNumber()
         ]);
     }
