984f76d6d3ad305a57e426ce4681f9f869ee2bdf
ahmadbaihaqi05 <ahmadbaihaqi05@student.ub.ac.id>
2024-07-24 13:30:29 +0700

    feat(ui):page-informasi

--- app/Http/Controllers/DataDesaController.php
+++ app/Http/Controllers/DataDesaController.php
@@ -11,12 +11,14 @@ class DataDesaController extends Controller
         $client = new Client();
        
         $response1 = $client -> request ('GET', env("API_BASE_URL", "http://localhost:8001") . "/api/data-desa/1");
+        $responseBody1 = json_decode($response1->getBody());
 
         $response2 = $client -> request ('GET', env("API_BASE_URL", "http://localhost:8001") . "/api/data-desa/2");
-
-        $responseBody1 = json_decode($response1->getBody());
         $responseBody2 = json_decode($response2->getBody());
 
-        return view("profilDesa",["data1"=>$responseBody1, "data2" => $responseBody2]);        
+        $response3 = $client -> request('GET', env("API_BASE_URL", "http://127.0.0.1:8001") . "/api/v-misi");
+        $responseBody3 = json_decode($response3->getBody());
+
+        return view("profilDesa",["data1"=>$responseBody1, "data2" => $responseBody2, "data3" => $responseBody3]);        
     }
 }
