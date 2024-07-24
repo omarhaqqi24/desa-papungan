cc7d9395354cdd4401536b77fae1aedff509fcb0
Rizki Fajar <muhrizkifajar28@gmail.com>
2024-07-24 17:49:55 +0700

    feat(ui): admin profil-desa styling input form

--- resources/views/components/admin-show.blade.php
+++ resources/views/components/admin-show.blade.php
@@ -6,26 +6,26 @@
 <div class="form-component">
     <label class="form-control w-full px-5 flex flex-col">
         <div class="label mb-1">
-            <span class="label-text font-medium">{{$judulPenjelasan}}</span>
-            <span class="label-text-alt">Top Right label</span>
+            <span class="label-text font-semibold">{{$judulPenjelasan}}</span>
+            <span class="label-text-alt font-medium">Top Right label</span>
         </div>
-        <textarea name="{{$nameTextarea}}" id="text-area-{{ $forValue }}" placeholder="Type here" class="input input-bordered w-full h-36 text-lg resize-none disabled:opacity-50" disabled >{{ $valueTextarea}}</textarea>
+        <textarea name="{{$nameTextarea}}" id="text-area-{{ $forValue }}" placeholder="Type here" class="input input-bordered w-full h-36 text-lg resize-none disabled:opacity-80 mb-2 py-4 disabled:border-2 disabled:border-gray-300 disabled:bg-gray-200" disabled >{{ $valueTextarea}}</textarea>
         <div class="flex-col justify-between w-full">
-            <div class="">{{ $subPenjelasan }}</div>
+            <p class="label-text font-medium text-gray-500">{{ $subPenjelasan }}</p>
         </div>    
     </label>
 
 
 
     <!-- Form Input foto  -->
-    <label class="form-control w-full px-5 flex flex-col">
+    <label class="form-control w-full px-5 flex flex-col mt-6">
         <div class="label mb-1">
-            <span class="label-text font-medium">Foto</span>
+            <span class="label-text font-semibold">Foto</span>
             <span class="label-text-alt">Top Right label</span>
         </div>
-        <input name="{{$nameInputPhoto}}" id="file-input-{{ $forValue }}" type="file" class="file-input file-input-bordered w-full mb-2 disabled:opacity-50" value="{{$valueFoto}}" disabled />
+        <input name="{{$nameInputPhoto}}" id="file-input-{{ $forValue }}" type="file" class="file-input file-input-bordered w-full mb-2 disabled:opacity-80 disabled:border-2 disabled:border-gray-300 disabled:bg-gray-200" value="{{$valueFoto}}" disabled />
         <div class="flex-col justify-between w-full">
-            <div class="">* file png, jpeg, dan jpg</div>
+            <p class="label-text font-medium text-gray-500"><span class="text-red-500">*</span> file png, jpeg, dan jpg</p>
         </div>    
     </label>
 
