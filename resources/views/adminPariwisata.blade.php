7a1158c725c82cb2c41a3fcf61bfd01d3bb913db
Rizki Fajar <muhrizkifajar28@gmail.com>
2024-07-25 08:36:03 +0700

    feat(ui): add admin page form

---
+++ resources/views/adminPariwisata.blade.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    @vite('resources/css/app.css')
+
+</head>
+
+<body class="mytheme font-jakarta antialiased dark:text-white/50">
+    <x-admin-navbar />
+
+    <div class="w-screen pl-32 py-3 h-20 bg-secondary text-lightText">
+        <div class="text-3xl font-bold">Pariwisata Desa Papungan</div>
+        <div class="">Home / Pariwisata Desa</div>
+    </div>
+
+    <div class="w-screen pl-32 pr-24 mt-5 space-y-4 mb-10">
+        <div class="w-1/2 flex items-center gap-2">
+            <div class="font-medium">Pariwisata</div>
+            <div class="flex-grow border-b-2 border-gray-500"></div>
+        </div>
+
+        <div class="w-full">
+            <div class="text-3xl font-semibold text-darkText">Daftar Foto Pariwisata</div>
+            <div class="py-2 text-gray-500">Berikut adalah penjelasan dari struktur organisasi yang ditampilkan</div>
+            <div class="relative overflow-x-auto border border-gray-300 rounded-2xl mt-6">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <caption class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">Daftar Foto</caption>
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Nama
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Alamat
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Nomor Kontak
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                <span class="sr-only">Edit</span>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <td class="px-6 py-4">
+                                Lembaga
+                            </td>
+                            <td class="px-6 py-4">
+                                Jalan
+                            </td>
+                            <td class="px-6 py-4">
+                                Nomor Kontak
+                            </td>
+                            <td class="px-6 py-4 text-right">
+                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            </td>
+                        </tr>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <td class="px-6 py-4">
+                                Lembaga
+                            </td>
+                            <td class="px-6 py-4">
+                                Jalan
+                            </td>
+                            <td class="px-6 py-4">
+                                Nomor Kontak
+                            </td>
+                            <td class="px-6 py-4 text-right">
+                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            </td>
+                        </tr>
+                        <tr class="bg-white dark:bg-gray-800">
+                            <td class="px-6 py-4">
+                                Lembaga
+                            </td>
+                            <td class="px-6 py-4">
+                                Jalan
+                            </td>
+                            <td class="px-6 py-4">
+                                Nomor Kontak
+                            </td>
+                            <td class="px-6 py-4 text-right">
+                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <section class="px-4 py-6 bg-white border-t">
+                    <div class="flex items-center justify-between w-full">
+                        <button disabled
+                            class="flex border border-gray-300 items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                            type="button">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                            aria-hidden="true" class="w-4 h-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"></path>
+                            </svg>
+                            Previous
+                        </button>
+                        <div class="flex items-center gap-2">
+                            <button
+                            class="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-lg bg-blue-100 text-center align-middle font-sans text-xs font-semibold uppercase text-darkText transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                            type="button">
+                                <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                                    1
+                                </span>
+                            </button>
+                            <button
+                            class="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                            type="button">
+                            <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                                2
+                            </span>
+                            </button>
+                            <button
+                            class="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                            type="button">
+                            <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                                3
+                            </span>
+                            </button>
+                            <button
+                            class="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                            type="button">
+                            <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                                4
+                            </span>
+                            </button>
+                            <button
+                            class="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                            type="button">
+                            <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                                5
+                            </span>
+                            </button>
+                        </div>
+                        <button
+                            class="flex border border-gray-300 items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                            type="button">
+                            Next
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                            aria-hidden="true" class="w-4 h-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"></path>
+                            </svg>
+                        </button>
+                    </div> 
+                </section>
+            </div>
+            <div class="flex justify-end mt-4">
+                <button class="btn text-lightText bg-secondary hover:bg-blue-900 px-4 py-2 rounded-xl flex items-center" onclick="i.showModal()">
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" class="mr-2">
+                        <path
+                            d="M13.375 4.625C13.875 4.125 13.875 3.375 13.375 2.875L11.125 0.625C10.625 0.125 9.875 0.125 9.375 0.625L0 10V14H4L13.375 4.625ZM10.25 1.5L12.5 3.75L10.625 5.625L8.375 3.375L10.25 1.5ZM1.25 12.75V10.5L7.5 4.25L9.75 6.5L3.5 12.75H1.25Z"
+                            fill="white" />
+                    </svg>
+                    Tambahkan
+                </button>
+            </div>
+            <hr class="h-px my-8 bg-gray-300 border-0">
+        </div>
+    </div>
+</body>
+
+</html>
--- resources/views/components/admin-show.blade.php
+++ resources/views/components/admin-show.blade.php
@@ -1,35 +1,28 @@
 <!-- resources/views/components/editable-form.blade.php -->
 
-
-
     <!-- Form Input TextArea Penjelasan -->
 <div class="form-component">
-    <label class="form-control w-full px-5 flex flex-col">
+    <label class="form-control w-full flex flex-col">
         <div class="label mb-1">
             <span class="label-text font-semibold">{{$judulPenjelasan}}</span>
             <span class="label-text-alt font-medium">Top Right label</span>
         </div>
-        <textarea name="{{$nameTextarea}}" id="text-area-{{ $forValue }}" placeholder="Type here" class="input input-bordered w-full h-36 text-lg resize-none disabled:opacity-80 mb-2 py-4 disabled:border-2 disabled:border-gray-300 disabled:bg-gray-200" disabled >{{ $valueTextarea}}</textarea>
+        <textarea name="{{$nameTextarea}}" id="text-area-{{ $forValue }}" placeholder="Type here" class="input input-bordered w-full h-36 text-lg resize-none disabled:opacity-80 mb-2 py-4 disabled:border disabled:border-gray-300 disabled:bg-slate-100" disabled >{{ $valueTextarea}}</textarea>
         <div class="flex-col justify-between w-full">
             <p class="label-text font-medium text-gray-500">{{ $subPenjelasan }}</p>
         </div>    
     </label>
 
-
-
     <!-- Form Input foto  -->
-    <label class="form-control w-full px-5 flex flex-col mt-6">
+    <label class="form-control w-full flex flex-col mt-6 {{ $nameInputPhoto ? '' : 'hidden' }}">
         <div class="label mb-1">
             <span class="label-text font-semibold">Foto</span>
             <span class="label-text-alt">Top Right label</span>
         </div>
-        <input name="{{$nameInputPhoto}}" id="file-input-{{ $forValue }}" type="file" class="file-input file-input-bordered w-full mb-2 disabled:opacity-80 disabled:border-2 disabled:border-gray-300 disabled:bg-gray-200" value="{{$valueFoto}}" disabled />
+        <input name="{{$nameInputPhoto}}" id="file-input-{{ $forValue }}" type="file" class="file-input file-input-bordered w-full mb-2 disabled:opacity-80 disabled:border disabled:border-gray-300 disabled:bg-slate-100" value="{{$valueFoto}}" disabled />
         <div class="flex-col justify-between w-full">
             <p class="label-text font-medium text-gray-500"><span class="text-red-500">*</span> file png, jpeg, dan jpg</p>
         </div>    
     </label>
-
-
-
     
 </div>
