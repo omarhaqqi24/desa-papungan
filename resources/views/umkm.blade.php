d200efb345a8aa86fd80a0ad080fc58db5f0f641
Jaya Winata <jayawinata100@gmail.com>
2024-07-28 02:34:07 +0700

    fix(UI): fix the interaction between umkm-page and peta-umkm

--- resources/views/umkm.blade.php
+++ resources/views/umkm.blade.php
@@ -31,12 +31,12 @@
         <div class="container items-center mx-auto space-y-10 text-justify">
             <x-headerArtikel subJudul="Kenali UMKM " judul="Profil UMKM Desa Papungan" />
             <div class="text-sm font-normal ">Berikut adalah video singkat mengenai UMKM di Desa Papungan. Simak video
-                berikut untuk mengetahui bagaimana masyarakat Desa Papungan mengembangkan potensi usaha mereka!/div>
+                berikut untuk mengetahui bagaimana masyarakat Desa Papungan mengembangkan potensi usaha mereka!</div>
 
-                <x-cardSubjudul jenisJudul="Kenali UMKM" judul="Peta Digital UMKM Desa Papungan"
-                    deskripsi="Berikut adalah Peta UMKM dari Desa Papungan. Simak informasi berikut untuk mengetahui lokasi dan jenis usaha yang ada di desa  kita, serta bagaimana UMKM ini berkontribusi dalam perekonomian lokal!" />
+            <x-cardSubjudul jenisJudul="Kenali UMKM" judul="Peta Digital UMKM Desa Papungan"
+                deskripsi="Berikut adalah Peta UMKM dari Desa Papungan. Simak informasi berikut untuk mengetahui lokasi dan jenis usaha yang ada di desa  kita, serta bagaimana UMKM ini berkontribusi dalam perekonomian lokal!" />
 
-            </div>
+        </div>
 
         <!-- peta umkm-->
         <div id="peta"></div>
@@ -45,140 +45,129 @@
                 id="petaumkm"></iframe>
         </div>
 
-
-            <!-- peta umkm-->
-            <div id="petaumkm"></div>
-
-
+        <!-- peta umkm-->
+        <div id="petaumkm"></div>
 
         <!-- daftar umkm-->
         <div id="daftarumkm"></div>
 
+        <!-- Search -->
+        <form action="{{ route('umkm.index') }}" method="GET">
+            <div class="flex items-center space-x-2 md:w-1/2 lg:w-1/4 hover:border-gray-400">
+                <label class="input input-bordered flex items-center gap-2 border-gray-400 bg-white">
+                    <input name="nama" type="search" class="grow" placeholder="Search" />
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                        class="h-4 w-4 opacity-70">
+                        <path fill-rule="evenodd"
+                            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                            clip-rule="evenodd" />
+                    </svg>
+                </label>
 
-            <!-- Search -->
-            <form action="{{ route('umkm.index') }}" method="GET">
-                <div class="flex items-center space-x-2 md:w-1/2 lg:w-1/4 hover:border-gray-400">
-                    <label class="input input-bordered flex items-center gap-2 border-gray-400 bg-white">
-                        <input name="nama" type="search" class="grow" placeholder="Search" />
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-                            class="h-4 w-4 opacity-70">
-                            <path fill-rule="evenodd"
-                                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                                clip-rule="evenodd" />
-                        </svg>
-                    </label>
-
-                    <div>
-                        <select name="jenis" id="jenis" class=" h-12 w-auto px-2 rounded-lg">
-                            <option disabled selected>Jenis UMKM</option>
-                            @foreach ($data->data->list as $item)
-                                <option value="{{$item->jenis}}">{{ $item->jenis }}</option>
-                            @endforeach
-                            <option value="">-</option>
-                        </select>
-                    </div>
-                    </details>
-
-                    <button type="submit" class="btn bg-secondary text-lightText hover:bg-blue-800">Search</button>
+                <div>
+                    <select name="jenis" id="jenis" class=" h-12 w-auto px-2 rounded-lg">
+                        <option disabled selected>Jenis UMKM</option>
+                        @foreach ($data->data->list as $item)
+                            <option value="{{ $item->jenis }}">{{ $item->jenis }}</option>
+                        @endforeach
+                        <option value="">-</option>
+                    </select>
                 </div>
-            </form>
+                </details>
 
+                <button type="submit" class="btn bg-secondary text-lightText hover:bg-blue-800">Search</button>
+            </div>
+        </form>
 
-
-
-
-
-
-
-
-            <!-- Tabel umkm -->
-            <x-table.table :headers="['Nama UMKM', 'Jenis Produk', 'Alamat', 'Ijin Label', 'Detail UMKM']" jenisTabel="Daftar UMKM">
-                @php
-                    $i = 1 + ($paginatedItems->currentPage() - 1) * $paginatedItems->perPage();
-                @endphp
-                @foreach ($paginatedItems as $umkm)
-                    <tr class="border-b">
-                        <td class="p-2 text-center">{{ $i++ }}</td>
-                        <td class="p-2 text-left">{{ $umkm->nama }}</td>
-                        <td class="p-2 text-left">
-                            @php
-                                $jenisCollection = collect($umkm->jenis);
-                                $jenisString = $jenisCollection->pluck('jenis')->unique()->implode(', ');
-                            @endphp
-                            {{ $jenisString }}
-                        </td>
-                        <td class="p-2 text-left">{{ $umkm->alamat }}</td>
-                        <td class="p-2 text-center lg:text-left ">
-                            @if ($umkm->no_pirt != '-')
-                                <div class="badge bg-indigo-100 text-indigo-400 font-semibold border-0 text-xs ">P-IRT
-                                </div>
-                            @endif
-                            @if ($umkm->no_nib != '-')
-                                <div class="badge bg-blue-100 text-blue-400 font-semibold border-0 text-xs ">NIB</div>
-                            @endif
-                            @if ($umkm->no_bpom != '-')
-                                <div class="badge bg-pink-100 text-pink-400 font-semibold border-0 text-xs ">BPOM</div>
-                            @endif
-                            @if ($umkm->no_halal != '-')
-                                <div class="badge bg-green-100 text-green-400 font-semibold border-0 text-xs ">HALAL
-                                </div>
-                            @endif
-                        </td>
-                        <td class="p-2 "><a class="btn btn-link" href="">Detail UMKM</a></td>
-                    </tr>
-                @endforeach
-            </x-table.table>
+        <!-- Tabel umkm -->
+        <x-table.table :headers="['Nama UMKM', 'Jenis Produk', 'Alamat', 'Ijin Label', 'Detail UMKM']" jenisTabel="Daftar UMKM">
+            @php
+                $i = 1 + ($paginatedItems->currentPage() - 1) * $paginatedItems->perPage();
+            @endphp
+            @foreach ($paginatedItems as $umkm)
+                <tr class="border-b">
+                    <td class="p-2 text-center">{{ $i++ }}</td>
+                    <td class="p-2 text-left">{{ $umkm->nama }}</td>
+                    <td class="p-2 text-left">
+                        @php
+                            $jenisCollection = collect($umkm->jenis);
+                            $jenisString = $jenisCollection->pluck('jenis')->unique()->implode(', ');
+                        @endphp
+                        {{ $jenisString }}
+                    </td>
+                    <td class="p-2 text-left">{{ $umkm->alamat }}</td>
+                    <td class="p-2 text-center lg:text-left ">
+                        @if ($umkm->no_pirt != '-')
+                            <div class="badge bg-indigo-100 text-indigo-400 font-semibold border-0 text-xs ">P-IRT
+                            </div>
+                        @endif
+                        @if ($umkm->no_nib != '-')
+                            <div class="badge bg-blue-100 text-blue-400 font-semibold border-0 text-xs ">NIB</div>
+                        @endif
+                        @if ($umkm->no_bpom != '-')
+                            <div class="badge bg-pink-100 text-pink-400 font-semibold border-0 text-xs ">BPOM</div>
+                        @endif
+                        @if ($umkm->no_halal != '-')
+                            <div class="badge bg-green-100 text-green-400 font-semibold border-0 text-xs ">HALAL
+                            </div>
+                        @endif
+                    </td>
+                    <td class="p-2 "> <a class="btn btn-link" href="#petaumkm"
+                            data-umkm-id="{{ $umkm->id }}">Detail UMKM</a></td>
+                </tr>
+            @endforeach
+        </x-table.table>
 
         <!-- Pagination Links -->
         <div class="flex justify-center mt-4">
-                @if ($paginatedItems->hasPages())
-                    <div class="join justify-between w-full">
-                        @if ($paginatedItems->onFirstPage())
-                            <button class="btn rounded-lg min-w-20" disabled>Previous</button>
-                        @else
-                            <a href="{{ $paginatedItems->previousPageUrl() }}"
-                                class="btn rounded-lg min-w-20 justify-between text-right pl-8 flex items-center relative group hover:bg-primary hover:text-white transition duration-300">
-                                <img src="img/leftArrowLogo.svg" alt="leftArrowLogo"
-                                    class="absolute left-2 group-hover:hidden">
-                                <img src="img/leftArrowHoverLogo.svg" alt="leftArrowHoverLogo"
-                                    class="absolute left-2 opacity-0 group-hover:opacity-100 transition duration-300">
-                                Previous
-                            </a>
-                        @endif
+            @if ($paginatedItems->hasPages())
+                <div class="join justify-between w-full">
+                    @if ($paginatedItems->onFirstPage())
+                        <button class="btn rounded-lg min-w-20" disabled>Previous</button>
+                    @else
+                        <a href="{{ $paginatedItems->previousPageUrl() }}"
+                            class="btn rounded-lg min-w-20 justify-between text-right pl-8 flex items-center relative group hover:bg-primary hover:text-white transition duration-300">
+                            <img src="img/leftArrowLogo.svg" alt="leftArrowLogo"
+                                class="absolute left-2 group-hover:hidden">
+                            <img src="img/leftArrowHoverLogo.svg" alt="leftArrowHoverLogo"
+                                class="absolute left-2 opacity-0 group-hover:opacity-100 transition duration-300">
+                            Previous
+                        </a>
+                    @endif
 
-                        <div class="flex space-x-2 justify-center flex-grow overflow-x-auto">
-                            @foreach ($paginatedItems->getUrlRange(1, $paginatedItems->lastPage()) as $page => $url)
-                                @if ($page <= 10 || $page >= $paginatedItems->lastPage() - 9)
-                                    @if ($page == $paginatedItems->currentPage())
-                                        <button
-                                            class="btn btn-square bg-primary text-lightText hover:bg-primary transition duration-300 hover:text-lightText active:bg-blue-900 active:text-lightText">{{ $page }}</button>
-                                    @else
-                                        <a href="{{ $url }}"
-                                            class="btn btn-square bg-transparent hover:bg-primary transition duration-300 hover:text-lightText active:bg-blue-900 active:text-lightText">{{ $page }}</a>
-                                    @endif
-                                @elseif ($page == 11 || $page == $paginatedItems->lastPage() - 10)
+                    <div class="flex space-x-2 justify-center flex-grow overflow-x-auto">
+                        @foreach ($paginatedItems->getUrlRange(1, $paginatedItems->lastPage()) as $page => $url)
+                            @if ($page <= 10 || $page >= $paginatedItems->lastPage() - 9)
+                                @if ($page == $paginatedItems->currentPage())
                                     <button
-                                        class="btn btn-square bg-transparent hover:bg-primary transition duration-300 hover:text-lightText active:bg-blue-900 active:text-lightText btn-disabled">...</button>
+                                        class="btn btn-square bg-primary text-lightText hover:bg-primary transition duration-300 hover:text-lightText active:bg-blue-900 active:text-lightText">{{ $page }}</button>
+                                @else
+                                    <a href="{{ $url }}"
+                                        class="btn btn-square bg-transparent hover:bg-primary transition duration-300 hover:text-lightText active:bg-blue-900 active:text-lightText">{{ $page }}</a>
                                 @endif
-                            @endforeach
-                        </div>
-
-                        @if ($paginatedItems->hasMorePages())
-                            <a href="{{ $paginatedItems->nextPageUrl() }}"
-                                class="btn rounded-lg min-w-20 justify-between flex items-center relative group hover:bg-primary hover:text-white transition duration-300">
-                                Next
-                                <img src="img/rightArrowLogo.svg" alt="rightArrowLogo"
-                                    class="absolute right-2 group-hover:hidden">
-                                <img src="img/rightArrowHoverLogo.svg" alt="rightArrowHoverLogo"
-                                    class="absolute right-2 opacity-0 group-hover:opacity-100 transition duration-300">
-                            </a>
-                        @else
-                            <button class="btn rounded-lg min-w-20" disabled>Next</button>
-                        @endif
+                            @elseif ($page == 11 || $page == $paginatedItems->lastPage() - 10)
+                                <button
+                                    class="btn btn-square bg-transparent hover:bg-primary transition duration-300 hover:text-lightText active:bg-blue-900 active:text-lightText btn-disabled">...</button>
+                            @endif
+                        @endforeach
                     </div>
-                @endif
-            </div>
-</div>
+
+                    @if ($paginatedItems->hasMorePages())
+                        <a href="{{ $paginatedItems->nextPageUrl() }}"
+                            class="btn rounded-lg min-w-20 justify-between flex items-center relative group hover:bg-primary hover:text-white transition duration-300">
+                            Next
+                            <img src="img/rightArrowLogo.svg" alt="rightArrowLogo"
+                                class="absolute right-2 group-hover:hidden">
+                            <img src="img/rightArrowHoverLogo.svg" alt="rightArrowHoverLogo"
+                                class="absolute right-2 opacity-0 group-hover:opacity-100 transition duration-300">
+                        </a>
+                    @else
+                        <button class="btn rounded-lg min-w-20" disabled>Next</button>
+                    @endif
+                </div>
+            @endif
+        </div>
+    </div>
 
 </body>
 <x-footer />
