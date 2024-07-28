6e5a5f4b17b36d0c43a6807488e39bb7c022bb59
Asti Syafitri <130469962+PlainIceTea@users.noreply.github.com>
2024-07-28 14:54:35 +0700

    feat(ui):umkm merapihkan paginator

--- resources/views/umkm.blade.php
+++ resources/views/umkm.blade.php
@@ -131,7 +131,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"></path>
                             </svg>
-                            Sebelumnya
+                            <div class="hidden md:block">Sebelumnya</div>
                         </button>
                     @else
                         <a href="{{ $paginatedItems->previousPageUrl() }}"
@@ -142,7 +142,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"></path>
                             </svg>
-                            Sebelumnya
+                            <div class="hidden md:block">Sebelumnya</div>
                         </a>
                     @endif
 
@@ -172,7 +172,7 @@
                         <a href="{{ $paginatedItems->nextPageUrl() }}"
                             class="flex border border-gray-300 items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-gray-900/20"
                             type="button">
-                            Berikutnya
+                            <div class="hidden md:block">Berikutnya</div>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                 stroke-width="2" stroke="currentColor" aria-hidden="true" class="w-4 h-4">
                                 <path stroke-linecap="round" stroke-linejoin="round"
@@ -183,7 +183,7 @@
                         <button disabled
                             class="flex border border-gray-300 items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                             type="button">
-                            Berikutnya
+                            <div class="hidden md:block">Berikutnya</div>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                 stroke-width="2" stroke="currentColor" aria-hidden="true" class="w-4 h-4">
                                 <path stroke-linecap="round" stroke-linejoin="round"
