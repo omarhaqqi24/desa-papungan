1523dae627c364812e3c3d5ea503627e30ded7c1
arvin-mf <arvinmulia55@gmail.com>
2024-07-28 17:09:56 +0700

    refactor(ui): include markdown in data desa update wev route

--- resources/views/adminProfilDesa.blade.php
+++ resources/views/adminProfilDesa.blade.php
@@ -75,7 +75,7 @@
                 <div class="form-control gap-4">
                     <label for="penjelasan" class="label-text font-semibold">Penjelasan</label>
                     <textarea disabled name="penjelasan" id="penjelasan"
-                        class="input input-bordered w-full py-4 h-36 disabled:bg-slate-100">{{ $profilDesa->data->penjelasan }}</textarea>
+                        class="input input-bordered w-full py-4 h-36 disabled:bg-slate-100">{{ $profilDesa->data->penjelasan_raw }}</textarea>
                 </div>
                 <div class="form-control gap-4">
                     <div tabindex="0" class="collapse collapse-arrow bg-slate-100 border border-gray-200 rounded-xl">
@@ -121,7 +121,7 @@
                             <div class="form-control gap-4">
                                 <label for="penjelasan" class="label-text font-semibold">Penjelasan</label>
                                 <textarea name="penjelasan" id="penjelasan"
-                                    class="input input-bordered w-full py-4 h-36 disabled:bg-slate-100">{{ $profilDesa->data->penjelasan }}</textarea>
+                                    class="input input-bordered w-full py-4 h-36 disabled:bg-slate-100">{{ $profilDesa->data->penjelasan_raw }}</textarea>
                             </div>
                             <div class="form-control gap-4">
                                 <label for="foto" class="label-text font-semibold">Foto</label>
@@ -364,7 +364,7 @@
                 <div class="form-control gap-4">
                     <label for="penjelasan" class="label-text font-semibold">Penjelasan</label>
                     <textarea disabled name="penjelasan" id="penjelasan"
-                        class="input input-bordered w-full py-4 h-36 disabled:bg-slate-100">{{ $sejarahDesa->data->penjelasan }}</textarea>
+                        class="input input-bordered w-full py-4 h-36 disabled:bg-slate-100">{{ $sejarahDesa->data->penjelasan_raw }}</textarea>
                 </div>
                 <div class="form-control gap-4">
                     <div tabindex="0" class="collapse collapse-arrow bg-slate-100 border border-gray-200 rounded-xl">
