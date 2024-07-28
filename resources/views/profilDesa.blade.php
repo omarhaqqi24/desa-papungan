257788d90b9f6f4e09c966232ed0a9689c1621cb
Jaya Winata <jayawinata100@gmail.com>
2024-07-29 02:18:14 +0700

    fix(UI): fix bg on visi and misi in profilDesa page

--- resources/views/profilDesa.blade.php
+++ resources/views/profilDesa.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ env("APP_NAME") . " | Profil Desa" }}</title>
+    <title>{{ env('APP_NAME') . ' | Profil Desa' }}</title>
     <link rel="icon" href="{{ asset('favicon.ico') }}">
     <style>
         .markdown-content ul {
