30b49428cb03af74a05d142c247911e87d2bb2f0
dvvnFrtn <muhrizkifajar28@gmail.com>
2024-08-12 22:22:02 +0700

    refactor: remove white and trailing space

--- resources/views/components/header-artikel.blade.php
+++ resources/views/components/header-artikel.blade.php
@@ -1,9 +1,9 @@
 <div>
-    <div class="mr-10 ">
-        <div class="container flex text-center items-center gap-5 w- md:w-1/2 lg:w-1/4 text-darkText">
-            <div class="text-sm font-medium whitespace-nowrap ">{{ $subJudul }}</div>
-            <div class=" border-b-2 border-black w-full"></div>
+    <div class="mr-10">
+        <div class="container flex text-center items-center gap-4 w-full md:w-1/2 lg:w-1/4 text-darkText">
+            <div class="text-lg font-medium whitespace-nowrap">{{ $subJudul }}</div>
+            <div class="border-b-2 border-black w-full"></div>
         </div>
-        <div class="text-2xl font-semibold">{{ $judul }}</div>
+        <div class="text-3xl font-semibold mt-4">{{ $judul }}</div>
     </div>
 </div>
