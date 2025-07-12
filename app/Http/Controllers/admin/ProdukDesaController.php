8b087ffb29f59c7ee8f7025a07fd046afd27a36e
RaffiAdyatma <adyatmasiub@student.ub.ac.id>
2025-07-12 08:32:58 +0700

    add dummy datas and pagination section

--- app/Http/Controllers/admin/ProdukDesaController.php
+++ app/Http/Controllers/admin/ProdukDesaController.php
@@ -4,22 +4,46 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ProdukDesaController extends Controller
 {
     public function index(Request $request){
 
-        // $produk1 = [
-        //     'nama' => 'blaa',
-        //     'jenis'=
-        //     'toko'
-            
-        // ]
+        $produk = [
+            'nama' => 'test',
+            'jenis'=> 'jenisan',
+            'toko' => 'down',
+            'alamat' => 'bumi',
+            'kontak' => '911',
+            'hargaRendah' => 5000,
+            'hargaTinggi' => 5001,
+            'desc' => 'awda awda awda'
+        ];
 
-        // $items = [
+        $items = array_fill(0, 22, $produk);
+        
+        $collection = collect($items);
 
-        // ]
+        $currentPage = LengthAwarePaginator::resolveCurrentPage();
+        $perPage = 10;
+
+        $currentPageItems = $collection->slice(($currentPage-1) * $perPage, $perPage)->all();
+
+        $paginatedItems = new LengthAwarePaginator(
+            $currentPageItems, 
+            $collection->count(),
+            $perPage,
+            $currentPage,
+            [
+                'path'  => $request->url(),     // URL dasar untuk link
+                'query' => $request->query()
+            ]
+            );
+
        
-        return view('adminProdukDesa');
+        return view('adminProdukDesa', [
+            'items' => $paginatedItems
+        ]);
     }
 }
